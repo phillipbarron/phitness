f5f1769b47aafb06a2eb9c7fb92b38e8e40b8002
Phill Barron <phillipbarron242@gmail.com>
2023-08-07 22:30:22 +0100

    auth route

--- apps/phitness-api/src/main.ts
+++ apps/phitness-api/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import express from 'express';
 import * as path from 'path';
 
@@ -14,7 +9,12 @@ app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to phitness-api!' });
 });
 
-const port = process.env.PORT || 3333;
+app.get('/authenticate/google', (req, res) => {
+  res.send({ message: 'Welcome to phitness-api!' });
+});
+
+const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
